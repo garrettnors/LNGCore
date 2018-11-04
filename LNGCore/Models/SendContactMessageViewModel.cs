52d0f2d07da1ccd1107124a1849fbb10755fbf54
Garrett Nors <garrett.nors@gmail.com>
2018-11-03 21:08:32 -0500

    Added unobtrusive validation for the contact page, removed all the junk in layout and made it much more clear

--- LNGCore/Models/SendContactMessageViewModel.cs
+++ LNGCore/Models/SendContactMessageViewModel.cs
@@ -8,11 +8,12 @@ namespace LNGCore.UI.Models
 {
     public class SendContactMessageViewModel
     {
-        [Required]
-        [MaxLength(100)]
+        [Required(ErrorMessage = "Please include your email address, we will need it to contact you back!")]
+        [StringLength(100, ErrorMessage = "Wow, your email address is really long! Sorry but it needs to be fewer than 100 characters.")]
+        [EmailAddress]
         public string EmailAddress { get; set; }
-        [Required]
-        [MaxLength(2000)]
+        [Required(ErrorMessage = "Please include a message, otherwise we won't know the intent of your inquiry!")]
+        [StringLength(2000, ErrorMessage = "What a novel! Please keep your message to fewer than 2000 characters.")]
         public string EmailMessage { get; set; }
     }
 }
